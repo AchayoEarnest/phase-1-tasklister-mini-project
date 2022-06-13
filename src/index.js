b25a31903a4b9ab5cff630d4139ddbe6a28c3c18
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-13 23:55:50 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('form').addEventListener('submit', (e)=>{
     e.preventDefault();
-    buildTodo(e.target.new-task-description.value)
+    buildTodo(e.target.task_list.value)
   })
 });
 
 
 function buildTodo(task){
   let li  = document.createElement('li');
+  // li.innerHTML = task_list.value;
   let btn = document.createElement('button');
   btn.addEventListener('click', handleDelete)
   btn.textContent = 'Delete'
-  li.textContent = task;
+  li.textContent = `${task} `;
   li.appendChild(btn)
   console.log(li)
   document.querySelector('#tasks').appendChild(li)
